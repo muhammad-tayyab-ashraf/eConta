78ed1d3462fc89843f9a0a0f18b89f1edc20c78c
Muhammad Tayyab <muhammad.tayyab@tkxel.com>
2022-08-18 01:25:40 +0500

    Updated design

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,4 +52,22 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.default_url_options = { :host => 'https://morning-garden-13764.herokuapp.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => "587",
+    :domain => "gmail.com",
+    :user_name => "muhammad.tayyab@tkxel.com",
+    :password => "tayyab711ta",
+    :authentication => "plain",
+    :enable_starttls_auto => true
+  }
 end
