06ea1d6bf1530450f0f73cd08de89b086e93f92c
Muhammad Tayyab <muhammad.tayyab@tkxel.com>
2022-08-19 00:04:13 +0500

    Final code refactoring

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,9 @@
 class HomeController < ApplicationController
   before_action :authenticate, except: [:welcome]
 
-  def welcome
-
-  end
+  def welcome;end
 
   private
-
   def authenticate
     redirect_to new_user_session_path unless user_signed_in?
   end
